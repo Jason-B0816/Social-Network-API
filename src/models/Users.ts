29b2e2c4d07d8742aedf23f48bc86073a2179fdb
Jason-B0816git config --global user.name Jason-B0816 <jbrooks200800@gmail.com>
2025-04-26 12:47:49 -0500

    updated file, deleted lines 45-58 code not needed.

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,41 +1,48 @@
 import { Schema, model, Types } from 'mongoose';
+// Removed unused import for reaction schema
 
-const usersSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must match a valid email address!'],
-  },
-  thoughts: [
-    {
-      type: Types.ObjectId,
-      ref: 'Thought',
+const usersSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-  ],
-  friends: [
-    {
-      type: Types.ObjectId,
-      ref: 'User',
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match a valid email address!'],
     },
-  ],
-}, {
-  toJSON: {
-    virtuals: true,
-    getters: true,
+    thoughts: [
+      {
+        type: Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Types.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
-  id: false,
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
+// Virtual for friend count
 usersSchema.virtual('friendCount').get(function () {
   return this.friends?.length;
 });
 
+
 const User = model('User', usersSchema);
+
 export default User;
